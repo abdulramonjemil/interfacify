5221199b96208e537d93f7d55436ac467a8031f4
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2022-10-31 08:13:01 +0100

    Begin implementation of interface

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,13 @@
 class Interface {
-  constructor(fields, attributes) {
+  static SIGNATURES = {
+    IS_VALID_FIELD_VALUE: Symbol("IS_VALID_FIELD_VALUE")
+  }
+
+  // constructor(fields, attributes) {
+  //   Interface.#assertConstructorArgumentsValidity(...arguments)
+  // }
+
+  static #assertConstructorArgumentsValidity(fields, attributes) {
     const fieldsTypeErrorMessage =
       "The definitions of fields of an interface (second argument passed to the `Interface` constructor) must be an ordinary object"
     if (typeof fields !== "object") throw new TypeError(fieldsTypeErrorMessage)
@@ -8,6 +16,7 @@ class Interface {
     if (fieldsPrototype !== Object.prototype && fieldsPrototype !== null)
       throw new TypeError(fieldsTypeErrorMessage)
 
+    if (attributes === undefined) return
     const attributesTypeErrorMessage =
       "The definitions of attributes of an interface (second argument passed to the `Interface` constructor) must be an ordinary object"
     if (typeof attributes !== "object")
