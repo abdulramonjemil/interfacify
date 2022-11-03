d74eadcfc4075d08354006a462d97c9e25eaf294
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2022-11-03 15:32:31 +0100

    Export necessary field types

--- field/types/index.js
+++ field/types/index.js
@@ -1,7 +1,13 @@
+const ArrayOf = require("./arrayof")
 const Default = require("./default")
+const InstanceOf = require("./instanceof")
+const OneOf = require("./oneof")
 const Primitive = require("./primitive")
 
 module.exports = {
+  ArrayOf,
   Default,
+  InstanceOf,
+  OneOf,
   Primitive
 }
