34ea10cb1e55d41a699686a01966dd0a0b5e5f90
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2022-11-07 17:49:04 +0100

    Add separate type to check exact values

---
+++ field/types/exact.js
@@ -0,0 +1,42 @@
+const BaseFieldType = require("./base")
+const { isSameValue, isSameValueZero } = require("../../lib/algorithms")
+
+class ExactFieldType extends BaseFieldType {
+  static #ADDITIONAL_ATTRIBUTES = [
+    { name: "isZeroSignIdentifier", default: false }
+  ]
+
+  constructor(determiner, attributes) {
+    if (determiner === undefined)
+      throw new TypeError(
+        "The 'determiner' of direct instances of 'ExactFieldType' cannot be 'undefined', " +
+          "make the field optional instead"
+      )
+    super(determiner, attributes)
+  }
+
+  static getSupportedAttributes() {
+    return [
+      ...BaseFieldType.DEFAULT_FIELD_ATTRIBUTES,
+      ...ExactFieldType.#ADDITIONAL_ATTRIBUTES
+    ]
+  }
+
+  get isZeroSignIdentifier() {
+    return this.$effectAttributeChaining("isZeroSignIdentifier")
+  }
+
+  isTypeOf(value) {
+    const {
+      isOptional: fieldIsOptional,
+      isZeroSignIdentifier: fieldIdentifiesZeroSigns
+    } = this.$attributes
+
+    if (value === undefined) return fieldIsOptional
+    return fieldIdentifiesZeroSigns
+      ? isSameValue(this.$DETERMINER, value)
+      : isSameValueZero(this.$DETERMINER, value)
+  }
+}
+
+module.exports = ExactFieldType
