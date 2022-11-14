dc645584d42f417347434cf8c0b9ad6c83dfde34
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2022-11-14 12:33:38 +0100

    Add definition for usable 'reusable field types'

--- field/index.js
+++ field/index.js
@@ -1,13 +1,42 @@
-const Field = {}
+const ReusableFieldType = require("./reusable-type")
+const FieldTypes = require("./types")
 
-Field.Types = {
-  bigint: "",
-  boolean: "",
-  function: "",
-  number: "",
-  object: "",
-  string: "",
-  symbol: ""
+const {
+  ArrayOf: ArrayOfFieldType,
+  Custom: CustomFieldType,
+  Exact: ExactFieldType,
+  InstanceOf: InstanceOfFieldType,
+  ObjectOf: ObjectOfFieldType,
+  OneOf: OneOfFieldType,
+  Primitive: PrimitiveFieldType
+} = FieldTypes
+
+class Field {
+  static Types = {
+    // Usable directly -e.g- 'Field.Types.string.isFilled.isOptional'
+    any: ReusableFieldType.create(new PrimitiveFieldType("any")),
+    array: ReusableFieldType.create(new PrimitiveFieldType("array")),
+    bigint: ReusableFieldType.create(new PrimitiveFieldType("bigint")),
+    boolean: ReusableFieldType.create(new PrimitiveFieldType("boolean")),
+    function: ReusableFieldType.create(new PrimitiveFieldType("function")),
+    number: ReusableFieldType.create(new PrimitiveFieldType("number")),
+    object: ReusableFieldType.create(new PrimitiveFieldType("object")),
+    string: ReusableFieldType.create(new PrimitiveFieldType("string")),
+    symbol: ReusableFieldType.create(new PrimitiveFieldType("symbol")),
+
+    // Called as functions -e.g- 'Field.Types.arrayOf(Field.Types.string)'
+    arrayOf: ReusableFieldType.provision(ArrayOfFieldType),
+    custom: ReusableFieldType.provision(CustomFieldType),
+    exact: ReusableFieldType.provision(ExactFieldType),
+    instanceOf: ReusableFieldType.provision(InstanceOfFieldType),
+    objectOf: ReusableFieldType.provision(ObjectOfFieldType),
+    oneOf: ReusableFieldType.provision(OneOfFieldType),
+    primitive: ReusableFieldType.provision(PrimitiveFieldType)
+  }
+
+  constructor() {
+    throw new Error("'Field' is not constructible")
+  }
 }
 
 module.exports = Field
