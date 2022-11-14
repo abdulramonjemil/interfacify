3c843d8af9794b690adc4aac1e8fc3e36831fc01
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2022-11-14 12:12:44 +0100

    Remove unnecessary definitions

--- field/index.js
+++ field/index.js
@@ -1,29 +1,13 @@
-const FieldTypes = require("./types")
-
-function createReusablePrimitiveFieldType(type) {
-  const createdFieldType = new FieldTypes.Primitive(type)
-
-  return new Proxy(createdFieldType, {
-    // get(target, property) {
-    //   const propertyIsFieldTypeAttribute =
-    //     SORTED_FIELD_ATTRIBUTES.includes(property)
-    //   if (propertyIsFieldTypeAttribute) {
-    //     assertEarlyAccessOfFieldTypeAttribute(target, property)
-    //   }
-    // }
-  })
-}
-
 const Field = {}
 
 Field.Types = {
-  bigint: createReusablePrimitiveFieldType("bigint"),
-  boolean: createReusablePrimitiveFieldType("boolean"),
-  function: createReusablePrimitiveFieldType("function"),
-  number: createReusablePrimitiveFieldType("number"),
-  object: createReusablePrimitiveFieldType("object"),
-  string: createReusablePrimitiveFieldType("string"),
-  symbol: createReusablePrimitiveFieldType("symbol")
+  bigint: "",
+  boolean: "",
+  function: "",
+  number: "",
+  object: "",
+  string: "",
+  symbol: ""
 }
 
 module.exports = Field
