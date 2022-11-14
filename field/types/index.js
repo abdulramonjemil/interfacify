b815c34362f5da4ae31547a9654dd220266d293b
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2022-11-14 12:19:31 +0100

    Export additional field types

--- field/types/index.js
+++ field/types/index.js
@@ -1,15 +1,21 @@
 const ArrayOf = require("./arrayof")
 const Base = require("./base")
+const Custom = require("./custom")
+const Exact = require("./exact")
 const InstanceOf = require("./instanceof")
 const ObjectOf = require("./objectof")
 const OneOf = require("./oneof")
 const Primitive = require("./primitive")
 
-module.exports = {
+const FieldTypes = {
   ArrayOf,
   Base,
+  Custom,
+  Exact,
   InstanceOf,
   ObjectOf,
   OneOf,
   Primitive
 }
+
+module.exports = FieldTypes
